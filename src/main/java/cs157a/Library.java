081252899b6d4d2625596698427dfec9bc445a89
Sambath <sambath.say@sjsu.edu>
2026-04-28 17:35:44 -0700

    Initial commit

--- src/main/java/cs157a/Library.java
+++ src/main/java/cs157a/Library.java
@@ -1,4 +1,9 @@
 package cs157a;
 
+import javax.swing.*;
+
 public class Library {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
+    }
 }
