3b34505d8c18dbf3f47856d335e0bf743b58ec2c
Sambath <sambath.say@sjsu.edu>
2026-05-04 10:42:31 -0700

    Fix BorrowRecord.java

--- src/main/java/cs157a/Payment.java
+++ src/main/java/cs157a/Payment.java
@@ -2,53 +2,84 @@ package cs157a;
 
 import java.time.LocalDate;
 
-public class Payment {
-    private long paymentId;
-    private long borrowRecordId; //Foreign key
-    private double paymentAmount;
-    private LocalDate paymentDate;
+public class BorrowRecord {
+    private long recordId;
+    private int bookId;
+    private int userId;
+    private LocalDate borrowDate;
+    private LocalDate dueDate;
+    private LocalDate returnDate;
+    private double fineAmount;
+
 
     //Constructor
-    public  Payment(){}
+    public  BorrowRecord(){}
 
-    public Payment(long paymentId, long borrowRecordId, double paymentAmount, LocalDate paymentDate){
-        this.paymentID = paymentId;
-        this.borrowRecordId = borrowRecordId;
-        this.paymentAmount = paymentAmount;
-        this.paymentDate = paymentDate;
+    public BorrowRecord(long recordId, int bookId, int userId, LocalDate borrowDate,LocalDate dueDate, LocalDate returnDate, double fineAmount){
+        this.recordId = recordId;
+        this.bookId = bookId;
+        this.userId = userId;
+        this.borrowDate = borrowDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+        this.fineAmount = fineAmount;
     }
 
     //Getters and Setters
-    public long getPaymentID(){
-        return paymentID;
+
+    public long getRecordId(){
+        return recordId;
+    }
+    public void setRecordId(long recordId){
+        this.recordId = recordId;
     }
 
-    public void setPaymentID(long paymentId){
-        this.paymentID = paymentId;
+    public int getBookId(){
+        return bookId;
     }
 
-    public long getBorrowRecordId(){
-        return borrowRecordId;
+    public void setBookId(int bookId){
+        this.bookId = bookId;
     }
 
-    public void setBorrowRecordId(long borrowRecordId){
-        this.borrowRecordId = borrowRecordId;
+    public int getUserId(){
+        return userId;
     }
 
-    public double getPaymentAmount(){
-        return paymentAmount;
+    public void setUserId(int userId){
+        this.userId = userId;
     }
 
-    public void setPaymentAmount(double paymentAmount){
-        this.paymentAmount = paymentAmount;
+    public LocalDate getBorrowDate(){
+        return borrowDate;
     }
 
-    public LocalDate getPaymentDate(){
-        return paymentDate;
+    public void setBorrowDate(LocalDate borrowDate){
+        this.borrowDate = borrowDate;
     }
 
-    public void setPaymentDate(LocalDate paymentDate){
-        this.paymentDate = paymentDate;
+    public LocalDate getDueDate(){
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate){
+        this.dueDate = dueDate;
+    }
+
+    public LocalDate getReturnDate(){
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate){
+        this.returnDate = returnDate;
+    }
+
+    public double getFineAmount(){
+        return fineAmount;
+    }
+
+    public void setFineAmount(double fineAmount){
+        this.fineAmount = fineAmount;
     }
 
 }
