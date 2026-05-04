12327c3953f6125dc7a6b8f6f26137f55f36c80d
Sneha <sneha.nalla@sjsu.edu>
2026-05-04 00:15:50 -0700

    Worked on BookDAO functions

--- src/main/java/cs157a/BookDAO.java
+++ src/main/java/cs157a/BookDAO.java
@@ -1,24 +1,220 @@
 package cs157a;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+
 public class BookDAO {
 
     // BookDAO.java
 
+    private List<Book> convertResultSet(ResultSet books) throws Exception{
+        List<Book> bookList = new ArrayList<Book>();
+        try(Connection conn = DatabaseConnection.getConnection()) {
+            while (books.next()) {
+                int bookid = books.getInt("BookID");
+                String status = books.getString("Status");
+                String isbn = books.getString("ISBN");
+                String title, author, genre;
+                if (isbn == null) {
+                    isbn = "";
+                    title = "";
+                    author = "";
+                    genre = "";
+                }
+                else {//fill in book info from ISBNs table
+                    try (PreparedStatement lookupISBN = conn.prepareStatement("SELECT * FROM ISBNs WHERE ISBN = ?")) {
+                        lookupISBN.setString(1, isbn);
+                        try (ResultSet book = lookupISBN.executeQuery()) {
+                            if (book.next()) {
+                                title = book.getString("Title");
+                                if (title == null) {
+                                    title = "";
+                                }
+                                author = book.getString("Author");
+                                if (author == null) {
+                                    author = "";
+                                }
+                                genre = book.getString("Genre");
+                                if (genre == null) {
+                                    genre = "";
+                                }
+                            } else {//no matching ISBN in ISBNs
+                                title = "";
+                                author = "";
+                                genre = "";
+                            }
+                        }
+                    }
+                }
+                bookList.add(new Book(bookid, title, author, genre, isbn, status));
+            }
+        }
+        return bookList;
+
+    }
+
     // Get all books from database
-    List<Book> getAllBooks();
+    List<Book> getAllBooks(){
+        List<Book> allBooks = new ArrayList<Book>();
+        try(Connection conn = DatabaseConnection.getConnection()){
+            try(PreparedStatement getBooks = conn.prepareStatement("SELECT * FROM Books")){
+                try (ResultSet books = getBooks.executeQuery()){
+                    allBooks = convertResultSet(books);
+                }
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            while(e.getNextException() != null){
+                e.printStackTrace();
+            }
+            throw new RuntimeException("Failed to fetch books. Database error.", e);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("Failed to fetch books", e);
+        }
+        return allBooks;
+    }
 
     // Search books by title, author, or genre
-    List<Book> searchBooks(String keyword, String searchType);
+    List<Book> searchBooks(String keyword, String searchType){
+        List<Book> searchResults = new ArrayList<Book>();
+        try(Connection conn = DatabaseConnection.getConnection()) {
+            Set<String> allowedColumns = Set.of("Title", "Author", "Genre");
+            if(allowedColumns.contains(searchType)) {
+                String query = "SELECT b.* FROM Books b JOIN ISBNs i ON b.ISBN = i.ISBN WHERE " + searchType + " LIKE ?";
+                try (PreparedStatement getResults = conn.prepareStatement(query)) {
+                    getResults.setString(1, "%" + keyword + "%");
+                    try (ResultSet books = getResults.executeQuery()) {
+                        searchResults = convertResultSet(books);
+                    }
+                }
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            while(e.getNextException() != null){
+                e.printStackTrace();
+            }
+            throw new RuntimeException("Failed to fetch books. Database error.", e);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("Failed to fetch books", e);
+        }
+        return searchResults;
+    }
 
     // Add a new book (and ISBN if needed)
-    boolean addBook(Book book);
+    boolean addBook(Book book){
+        try(Connection conn = DatabaseConnection.getConnection()){
+            try (PreparedStatement checkISBN = conn.prepareStatement("SELECT * FROM ISBNs WHERE ISBN = ?")) {
+                checkISBN.setString(1,book.getIsbn());
+                try(ResultSet match = checkISBN.executeQuery()){
+                    if(!match.next()){ //ISBN doesn't exists in ISBNs
+                        try (PreparedStatement insertISBN = conn.prepareStatement("INSERT INTO ISBNs (ISBN, Title, Author, Genre) VALUES ('?', '?', '?', '?')")) {
+                            insertISBN.setString(1, book.getIsbn());
+                            insertISBN.setString(2, book.getTitle());
+                            insertISBN.setString(3, book.getAuthor());
+                            insertISBN.setString(4, book.getGenre());
+                            insertISBN.executeUpdate();
+                        }
+                    }
+                    try (PreparedStatement insertBook = conn.prepareStatement("INSERT INTO Books (ISBN, Status) VALUES ('?', '?')")) {
+                        insertBook.setString(1, book.getIsbn());
+                        insertBook.setString(2, book.getStatus());
+                        insertBook.executeUpdate();
+                        return true;
+                    }
+                }
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            while(e.getNextException() != null){
+                e.printStackTrace();
+            }
+            throw new RuntimeException("Failed to add book. Database error.", e);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("Failed to add book", e);
+        }
+    }
 
     // Update an existing book
-    boolean updateBook(Book book);
+    boolean updateBook(Book book){
+        try(Connection conn = DatabaseConnection.getConnection()){
+            try(PreparedStatement getBook = conn.prepareStatement()){
+
+            }
+            try (PreparedStatement updateBook = conn.prepareStatement("UPDATE Books SET ISBN = ?, Status = ? WHERE BookID = ?")) {
+                updateBook.setString(1, book.getIsbn());
+                updateBook.setString(2, book.getStatus());
+                updateBook.setInt(3, book.getBookId());
+                int rowsAffected = updateBook.executeUpdate();
+                if(rowsAffected == 1){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            while(e.getNextException() != null){
+                e.printStackTrace();
+            }
+            throw new RuntimeException("Failed to update book. Database error.", e);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("Failed to update book", e);
+        }
+    }
 
     // Delete a book (only if not borrowed)
-    boolean deleteBook(int bookId);
-
-    // Change book status (Available/Borrowed/Damaged/Lost)
-    boolean updateBookStatus(int bookId, String status);
+    boolean deleteBook(int bookId){
+        try(Connection conn = DatabaseConnection.getConnection()){
+            try (PreparedStatement checkStatus = conn.prepareStatement("SELECT Status FROM Books WHERE BookID = ?")) {
+                checkStatus.setInt(1, bookId);
+                try(ResultSet statusSet = checkStatus.executeQuery()){
+                    if(statusSet.next()){
+                        String status = statusSet.getString("Status");
+                        if(status.equals("Borrowed")){
+                            return false;
+                        }
+                    }
+                }
+            }
+            try(PreparedStatement deleteBook = conn.prepareStatement("DELETE FROM Books WHERE BookID = ?")){
+                deleteBook.setInt(1, bookId);
+                int rowsAffected = deleteBook.executeUpdate();
+                if(rowsAffected == 1){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            while(e.getNextException() != null){
+                e.printStackTrace();
+            }
+            throw new RuntimeException("Failed to delete book. Database error.", e);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("Failed to delete book", e);
+        }
+    }
 }
